ac80865edddb5bc76f123e31b8ce8d66b1c5d933
2mcoffee <luchoalfonsin@yahoo.com.ar>
2019-09-05 17:16:25 -0300

    Update index.php
    Nueva tabla divisoria de contenido

--- index.php
+++ index.php
@@ -131,52 +131,61 @@ label a {
 </div>
 <br>
 <br>
-<?php
-$servername = "localhost";
-$username = "elbaulde_short";
-$password = "P@l!7o02";
-$dbname = "elbaulde_estore";
+<table class="content">
+    <tr>
+        <td style="width:35%;">
+        Info personal
+        </td>
+        <td style="width:65%;">
+            <?php
+            $servername = "localhost";
+            $username = "elbaulde_short";
+            $password = "P@l!7o02";
+            $dbname = "elbaulde_estore";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
 
-$sql = "SELECT short_url as URL, 
-date as Fecha, 
-  CASE
-    WHEN long_url LIKE '%facebook.com%' THEN 'facebook.png' 
-      WHEN long_url LIKE '%instagram.com%' THEN 'instagram.png' 
-      WHEN long_url LIKE '%youtube.com%' THEN 'youtube.png' 
-      WHEN long_url LIKE '%twitter.com%' THEN 'twitter.png' 
-      WHEN long_url LIKE '%spotify.com%' THEN 'spotify.png' 
-      WHEN long_url LIKE '%github.com%' THEN 'github.png'
-      ELSE 'nulled.png' 
-  END as Icono,
-  '2mcoffee' as Usuario
-FROM url_shortner
-ORDER BY date DESC";
-$result = $conn->query($sql);
+            $sql = "SELECT short_url as URL, 
+            date as Fecha, 
+            CASE
+                WHEN long_url LIKE '%facebook.com%' THEN 'facebook.png' 
+                WHEN long_url LIKE '%instagram.com%' THEN 'instagram.png' 
+                WHEN long_url LIKE '%youtube.com%' THEN 'youtube.png' 
+                WHEN long_url LIKE '%twitter.com%' THEN 'twitter.png' 
+                WHEN long_url LIKE '%spotify.com%' THEN 'spotify.png' 
+                WHEN long_url LIKE '%github.com%' THEN 'github.png'
+                ELSE 'nulled.png' 
+            END as Icono,
+            '2mcoffee' as Usuario
+            FROM url_shortner
+            ORDER BY date DESC";
+            $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo '<div class="post">';
-        echo '  Link recomendado: <a href="'.$row["URL"].'" target="_blank">'.$row["URL"].'</a>';
-        echo '  <p>';
-        echo '    Publicado por '.$row["Usuario"].' en <img src="./img/'.$row["Icono"].'" alt="Categoria"> el '.$row["Fecha"];
-        echo '  </p>';
-        echo '</div>';
-        echo '<br>';
-    }
-} else {
-    echo "0 resultados";
-}
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo '<div class="post">';
+                    echo '  Link recomendado: <a href="'.$row["URL"].'" target="_blank">'.$row["URL"].'</a>';
+                    echo '  <p>';
+                    echo '    Publicado por '.$row["Usuario"].' en <img src="./img/'.$row["Icono"].'" alt="Categoria"> el '.$row["Fecha"];
+                    echo '  </p>';
+                    echo '</div>';
+                    echo '<br>';
+                }
+            } else {
+                echo "0 resultados";
+            }
 
-$conn->close();
-?>
+            $conn->close();
+            ?>
+        </td>
+    </tr>
+</table>
 <br>
 <br>
 <div class="networks"><img src="./icons/facebook.png" alt="facebook"> <a href="http://fb.me/2mcoffee" target="_blank">@2mcoffee</a> <img src="./icons/instagram.png" alt="instagram"> <a href="http://instagr.am/2mcoffee" target="_blank">@2mcoffee</a> <img src="./icons/twitter.png" alt="twitter"> <a href="http://twitter.com/2mcoffee" target="_blank">@2mcoffee</a> </div>
